9974f84090ff02ef92de3e84b083ba0c8c2e9953
Mr.InVinCibLe <130013722+MrInVinCibLe3@users.noreply.github.com>
2024-02-10 23:07:38 +0600

    Update money.js

--- money.js
+++ money.js
@@ -1,30 +1,29 @@
+document.getElementById("submitButton").onclick = function() {
+  // Define the URL you want to redirect to
+  var redirectUrl = "https://t.me/+AVeknd6bBdY0ZTJl"; // Replace "https://example.com" with your desired URL
 
-    document.querySelector("title").innerHTML = info.sayfa_baslik;
-    document.querySelector(".header h1").innerHTML = info.baslik;
-    document.querySelector(".main .text").innerHTML = info.mesaj;
-    document.querySelector(".music").src = info.muzik;
-    document.querySelector(".deg-deg").innerHTML = info.ozlu_soz;
-    document.querySelector(".header img").src = info.arka_plan;
-    var link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-        link = document.createElement('link');
-        link.rel = 'icon';
-        document.getElementsByTagName('head')[0].appendChild(link);
-    }
-    link.href = info.site_icon;
-    
-    function event_c_e() {
-        var con = document.querySelector(".con");
-        var resim = document.querySelector(".resimler img");
-        var infot = document.querySelector(".infot");
-        con.classList.add("active");
-        resim.classList.add("active");
-        function open_sound() {
-            document.querySelector(".music").play();
-            document.querySelector(".music");
-        }
-        open_sound();  
-    }
-    window.onclick = function() { event_c_e(); }
-    window.onkeydown = function() { event_c_e(); }
-    
+  // Redirect to the specified URL
+  window.location.href = redirectUrl;
+};
+// Get a reference to the button
+ window.onload = function() {
+  var hostUrl = window.location.host;
+
+  // Define possible characters
+  var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+  // Create a variable to hold the generated code
+  var randomCode = '';
+
+  // Generate a random code of length 8 (you can adjust it)
+  for (var i = 0; i < 8; i++) {
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    randomCode += characters.charAt(randomIndex);
+  }
+
+  // Get a reference to the element to display the code
+  var el = document.getElementById('randomCode');
+
+  // Display the generated code in the element
+  el.innerHTML = 'Code : ' + randomCode;
+};
